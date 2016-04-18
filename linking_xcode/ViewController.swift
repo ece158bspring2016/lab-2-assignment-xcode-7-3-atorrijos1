b2880a22fcb00df83070d26b0c655a89d9961fbd
Ana Torrijos <Ana@anas-macbook-air-3.local>
2016-04-17 21:39:16 -0700

    Initial Commit

---
+++ linking_xcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  linking_xcode
+//
+//  Created by Ana Torrijos on 17/04/16.
+//  Copyright © 2016 Ana Torrijos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
